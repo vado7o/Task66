935565d5b527f0678493c3c86823a2d8adbc2fed
Vadim Pudov <pvb@rambler.ru>
2022-10-18 19:35:21 +0700

    all changes

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+Console.Clear();
+Console.WriteLine("Программа выдаёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии");
+
+int first = 0;
+int sum = 0;
+first = AskForInput("меньшее");
+int second = AskForInput("большее");
+
+System.Console.WriteLine("\nСумма всех натуральных чисел в промежутке от " + first + " до " + second + $": {PrintSumNumbers(first + 1, second)}");
+
+
+//////////////////////////// функции ////////////////////////////////////////////////////
+
+
+int AskForInput(string str)
+{
+    while (true)
+    {
+        Console.Write($"\nНапишите {str} натуральное число интервала :");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            if (number > 0 && number > first) 
+            {
+                return number;
+                break;
+            }
+            else Console.WriteLine($"Некорректно указано {str} натуральное число интервала!\n");
+        }
+        else Console.WriteLine($"Некорректно указано {str} натуральное число интервала!\n");
+    }
+}
+
+int PrintSumNumbers(int first, int second)
+{
+    return first == second - 1 ? first : PrintSumNumbers(first + 1, second) + first;
+}
+
